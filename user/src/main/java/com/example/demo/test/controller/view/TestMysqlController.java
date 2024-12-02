e51d905bd9d80a60938b923a9b7a5affddd24e2b
laijianping <535678291@qq.com>
2024-12-02 09:48:51 +0800

    mysql日志打印

---
+++ user/src/main/java/com/example/demo/test/controller/view/TestMysqlController.java
@@ -0,0 +1,41 @@
+package com.example.demo.test.controller.view;
+
+import com.example.demo.infruastructure.common.BaseResult;
+import com.example.demo.test.service.MongoDbTestService;
+import com.example.demo.test.service.MysqlTestService;
+import com.example.demo.test.service.TestService;
+import com.example.demo.user.dto.UserInfoDto;
+import com.example.demo.user.param.UserInfoParam;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/user/test")
+@Api(tags = "test")
+@Slf4j
+public class TestMysqlController {
+
+
+    @Resource
+    private MysqlTestService mysqlTestService;
+
+    @PostMapping("/testMysql/findByUsername")
+    public BaseResult<UserInfoDto> findByUsername(@RequestBody UserInfoParam param) {
+        log.info("testMysql接口");
+        BaseResult<UserInfoDto> result = new BaseResult<>();
+        result.setSuccess();
+        UserInfoDto userInfoDto = mysqlTestService.findByUsername(param.getUserName());
+        result.setData(userInfoDto);
+        return result;
+    }
+
+
+}
